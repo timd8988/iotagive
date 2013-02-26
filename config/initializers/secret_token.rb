1e143ba250760dc3e031794fafe6d676eb4f441a
tim duymich <timd8988@gmail.com>
2013-02-26 00:29:55 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iotagive::Application.config.secret_token = '9a5eda1656c528796f1ba3eaae9103cfe0f88f9ee1ce043b23abc017b2c0a1df75e127ef0b511e7a4b5c64a4b634fa4a3ce7ac42e6d51ac22124686550f19798'
