521ce491a858ad9bcc7b8371e09b8f5228ff3dd3
tim duymich <timd8988@gmail.com>
2013-02-27 16:43:57 -0800

    Add Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Iotagive::Application.routes.draw do
+ get 'about' => 'pages#about'
+ root :to => 'pages#home'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
